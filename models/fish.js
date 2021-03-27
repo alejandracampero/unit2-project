cf1e11696b8be745d45d3fb4dc955cff838c104c
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-03-27 10:50:53 -0400

    modify header

---
+++ models/fish.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fishSchema = new Schema({
+  species: {type: String},
+  imageUrl: {type: String},
+  description: {type: String},
+  care:{type: String}
+}, {
+  timestamps:true
+})
+module.exports = mongoose.model('Fish', fishSchema);
