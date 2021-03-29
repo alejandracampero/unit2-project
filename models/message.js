b2d8db0242a9ed280b44a206885ce7217e8531ad
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-03-29 09:40:42 -0400

    create message model

---
+++ models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const messageSchema = new Schema({
+  postedBy:[{type: Schema.Types.ObjectId, ref: 'User'}],
+  title:String,
+  replies:[replySchema]
+}, {
+  timestamps:true
+})
+
+module.exports = mongoose.model("Message", messageSchema)
+
